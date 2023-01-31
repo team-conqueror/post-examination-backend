0a9c55363233051610f5cb6786782dd338b42c46
NemindaPiyasena <nemindaprabhashwara@gmail.com>
2023-02-01 04:18:55 +0530

    BLOG-7: db connection configured

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import dotenv from 'dotenv';
+
 import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from '@apollo/server/standalone';
+import {connectDB} from "./db_client/connection";
 
+
+
+dotenv.config();
+
+const port: number = Number(process.env.PORT);
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = `#graphql
@@ -12,14 +20,34 @@ const typeDefs = `#graphql
 // Provide resolver functions for your schema fields
 const resolvers = {
     Query: {
-        hello: () => 'Hello world! 1',
+        hello: () => 'Hello world! 3',
     },
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
 
 const {url} = await startStandaloneServer(server, {
-    listen: {port: 4000},
+    listen: {port: port},
 });
 
 console.log(`🚀  Server ready at: ${url}`);
+
+
+
+const pool = connectDB();
+
+async function insertData() {
+    const [name, color] = ["blue", "red"];
+    try {
+        const res = await (await pool).query(
+            "INSERT INTO shark (name, color) VALUES ($1, $2)",
+            [name, color]
+        );
+        console.log(`Added a shark with the name ${name}`);
+    } catch (error) {
+        console.error(error)
+    }
+    console.log(name, color);
+}
+
+insertData().then(r => console.log('done inserting'))
