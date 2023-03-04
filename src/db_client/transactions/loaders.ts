3733356aa968ad665eb57abdfd299da5af692425
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2023-03-04 18:05:43 +0530

    BLOG-25: created voting mutation

--- src/db_client/transactions/loaders.ts
+++ src/db_client/transactions/loaders.ts
@@ -6,13 +6,19 @@ import {selectAllPostsQuery, selectPostByIdQuery} from "../queries/read/posts.js
 import {selectAllDataQuery, selectUserByIdQuery} from "../queries/read/users.js";
 import {selectAnswerByIdQuery, selectAnswersByPostIdQuery} from "../queries/read/answers.js";
 import {selectCommentByIdQuery, selectCommentByPostIdQuery} from "../queries/read/comments.js";
-import {selectVoteTypeByDocumentIdQuery} from "../queries/read/votes.js";
+import {selectUserVoteByDocumentIdQuery, selectVoteTypeByDocumentIdQuery} from "../queries/read/votes.js";
 import {PostCreateInputType} from "../../types/graphql_types/input/post.js";
 import {CommentCreateInputType} from "../../types/graphql_types/input/comment.js";
 import {createPostQuery} from "../queries/write/post.js";
 import {createCommentQuery} from "../queries/write/comment.js";
 import {AnswerCreateInputType} from "../../types/graphql_types/input/answer.js";
 import {createAnswerQuery} from "../queries/write/answer.js";
+import {VoteCreateInputType} from "../../types/graphql_types/input/vote.js";
+import {
+    alterUserVoteForDocumentIdQuery,
+    createVoteForDocumentIdQuery,
+    removeUserVoteForDocumentIdQuery
+} from "../queries/write/vote.js";
 
 
 export const getNodeById = async (nodeId: string) => {
@@ -131,6 +137,51 @@ export const createAnswer = async (answerCreateInput: AnswerCreateInputType): Pr
     const answer = await runQuery(createAnswerQuery, [answerCreateInput.body, postId, userId]);
 
     //ToDo: return the newly created answer
+}
+
+
+export const checkUserForDocumentVote = async (userId: string, documentType: string, documentId: string): Promise<any> => {
+
+    return await runQuery(selectUserVoteByDocumentIdQuery, [userId, documentType, documentId])
+        .then((result: any) => {
+            return result?.rows[0];
+        })
+
+}
+
+export const doesUserVoteTypeMatch = (dbVoteType: string, newVoteType: string): boolean => {
+    return dbVoteType === newVoteType
+}
+export const createVote = async (voteCreateInput: VoteCreateInputType): Promise<any> => {
+
+    const userVote = await checkUserForDocumentVote(voteCreateInput.userId, voteCreateInput.documentType, voteCreateInput.documentId);
+
+    if (userVote) {
+        console.log(userVote)
+
+        // ToDo: check for new request's vote type; if both vote types are same remove the record, or alter the vote type
+
+        if (doesUserVoteTypeMatch(userVote.vote_type, voteCreateInput.voteType)) {
+            // ToDo: remove the record
+
+            await runQuery(removeUserVoteForDocumentIdQuery, [voteCreateInput.userId, voteCreateInput.documentType, voteCreateInput.documentId]);
+            console.log('removed ')
+        } else {
+            // ToDo: update the record
+
+            await runQuery(alterUserVoteForDocumentIdQuery, [voteCreateInput.voteType, voteCreateInput.userId, voteCreateInput.documentType, voteCreateInput.documentId])
+            console.log('altered')
+        }
+    } else {
+        console.log(`no votes ${userVote}`)
+
+        // ToDo: create vote type for the user_id for the document_id
+
+        await runQuery(createVoteForDocumentIdQuery, [voteCreateInput.voteType, voteCreateInput.documentType, voteCreateInput.documentId, voteCreateInput.userId])
+        console.log('created new')
+
+    }
+
 }
 
 export const getAllUsers = async (): Promise<QueryResult<UserType> | any> => {
---
+++ src/graphql/schema/mutations/voteCreate.ts
@@ -0,0 +1,28 @@
+import {createVote} from "../../../db_client/transactions/loaders.js";
+import {VoteCreateInputType} from "../../../types/graphql_types/input/vote.js";
+
+export const typeDef = `#graphql
+
+    input VoteCreateInputType {
+        documentId: String!
+        documentType: String!
+        userId: String!
+        voteType: String!
+    }
+    
+    extend type Mutation {
+        createVote(voteCreateInput: VoteCreateInputType!): Node
+    }
+
+`;
+
+export const resolver = {
+
+    Mutation: {
+        createVote: async (_: null, args: { voteCreateInput: VoteCreateInputType }) => {
+            return await createVote(args.voteCreateInput);
+        }
+    }
+
+}
+
